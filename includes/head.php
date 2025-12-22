6b1a45f42a2df9e801b06d3f8fd50dcfe6983143
Ben Westover <me@benthetechguy.net>
2025-12-22 02:58:17 -0500

    rewrite: foundations

---
+++ includes/head.php
@@ -0,0 +1,11 @@
+<head>
+    <title><?php echo $title; ?></title>
+    <meta charset="utf-8">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://techsupportcentral.org/">
+    <meta property="og:title" content="Tech Support Central">
+    <meta property="og:description" content="Free and fast tech support server with over 1000 members">
+    <meta property="og:image" content="https://techsupportcentral.org/tsc.png">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/includes/styles.css">
+</head>
--- index.php
+++ index.php
@@ -1,52 +1,14 @@
-<html>
-	<head>
-		<title> Home | Tech Support Central </title>
-		<style>
-			#SideBar {
-				width: 375px;
-				height: 800px;
-				background-color: #D66225;
-				float: right;
-				text-align: center;
-			}
-
-			#MainBody {
-				width: 625px;
-				height: 800px;
-				background-color: #EEEEEE;
-				float: left;
-				text-align: center;
-			}
-		</style>
-		<?php include 'includes/head.html';?>
-	</head>
-	<body bgcolor="#323232">
-		<div id="Container">
-			<?php include 'includes/header.html';?>
-			<div id="SideBar">
-				<br>
-				<iframe src="https://discord.com/widget?id=824042976371277884&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
-			</div>
-			<div id="MainBody">
-				<h2 style="color: #D66225"> Welcome to Tech Support Central! </h2>
-				<br>
-				<h2> What we offer: </h2>
-				<h3 style="text-align: left; margin-left: 16px; margin-right: 16px">
-					<ul>
-						<li> Free and fast tech support for whatever your issue may be </li>
-						<br>
-						<li> Tech discussions for you to show off your personal setups </li>
-						<br>
-						<li> Shopping recommendations, from tablets to PCs to game consoles; whatever you need, we can help! </li>
-						<br>
-						<li> We're always looking for more support team members, so apply if you wish and help out others! </li>
-					</ul>
-				</h3>
-				<h3> We also have a <a href="https://matrix.org"> Matrix </a><a href="https://element.io/blog/spaces-blast-out-of-beta/">Space</a> at <a href="https://matrix.to/#/#techsupportcentral:matrix.org">#techsupportcentral:matrix.org</a>. </h3>
-				<br>
-				<iframe width="560" height="315" src="https://www.youtube.com/embed/FQYcyzx_lGo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-			</div>
-			<?php include 'includes/footer.html';?>
-		</div>
-	</body>
+<!DOCTYPE html>
+<html lang="en">
+<?php
+    $title = "Home | Tech Support Central";
+    include 'includes/head.php';
+?>
+<body>
+    <?php include 'includes/header.html'; ?>
+    <div id="content">
+        <h1 style="margin-top: 0; padding-top: 22px;">test</h1>
+    </div>
+    <?php include 'includes/footer.html'; ?>
+</body>
 </html>
